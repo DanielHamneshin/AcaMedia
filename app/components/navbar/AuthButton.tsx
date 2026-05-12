2799dc89a0dbe4d4b716b0e25ee95eab0230818c
דניאל המנשין <danham531@gmail.com>
2026-05-12 18:53:40 +0300

    refactor(project): move auth logic from NavBar to AuthButton

---
+++ app/components/navbar/AuthButton.tsx
@@ -0,0 +1,31 @@
+import { auth, signOut } from "@/auth";
+import Link from "next/link";
+
+const AuthButton = async () => {
+    const session = await auth();
+    const firstName = session?.user?.name?.split(" ")[0];
+    const lastName = session?.user?.name?.split(" ")[1];
+    return (
+        <div className="ml-auto flex items-center gap-2">
+            {session ?
+                <form action={
+                    async () => {
+                        "use server";
+                        await signOut({
+                            redirectTo: "/login"
+                        });
+                    }
+                }>
+                    <button type="submit" className=" flex items-center gap-2 cursor-pointer border border-neutral-200 rounded-full p-1 bg-neutral-100 dark:text-black hover:bg-neutral-300">
+                        <img src={`https://ui-avatars.com/api/?name=${firstName}+${lastName || ""}`} alt="" className="w-8 h-8 bg-white border border-gray-200 rounded-full" />
+                    </button>
+                </form> :
+                <Link href="/login" className="flex items-center gap-2 cursor-pointer border border-neutral-200 rounded-full p-1 bg-neutral-100 dark:text-black hover:bg-neutral-300" >
+                    <img src="/github-svgrepo-com.svg" alt="" className="w-8 h-8 bg-white border border-gray-200 rounded-full" />
+                    Login
+                </Link>}
+        </div>
+    )
+}
+
+export default AuthButton
