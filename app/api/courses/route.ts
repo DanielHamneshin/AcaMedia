ac312e7b86244f856a1a105690d7d178c96def29
דניאל המנשין <danham531@gmail.com>
2026-06-03 00:49:40 +0300

    feat(api): create course member basic functions and endoints

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -2,12 +2,26 @@ import { createCourseService, getAllCoursesService } from "@/server/courses/cour
 
 
 
-export const POST = async () => {
-    const newCourse = await createCourseService({ title: "Test", description: "Test" });
-    return Response.json(newCourse);
+export const POST = async (request: Request) => {
+    try {
+        const { title, description } = await request.json();
+        const newCourse = await createCourseService({ title, description });
+        return Response.json(newCourse, { status: 200 });
+    }
+    catch (error) {
+        console.error(error);
+        return Response.json({ error: "Error creating course " + error }, { status: 500 });
+    }
+
 }
 
 export const GET = async () => {
-    const courses = await getAllCoursesService();
-    return Response.json(courses);
+    try {
+        const courses = await getAllCoursesService();
+        return Response.json(courses, { status: 200 });
+    }
+    catch (error) {
+        console.error(error);
+        return Response.json({ error: "Error getting all courses " + error }, { status: 500 });
+    }
 }
--- server/courses/course.service.ts
+++ server/courses/course.service.ts
@@ -2,14 +2,63 @@ import prisma from "@/lib/prisma";
 
 
 export const createCourseService = async ({ title, description }: { title: string, description: string }) => {
-    return await prisma.course.create({
-        data: {
-            title,
-            description
-        }
-    });
+    try {
+        const newCourse = await prisma.course.create({
+            data: {
+                title,
+                description
+            }
+        });
+        return newCourse;
+    }
+    catch (error) {
+        console.error(error);
+        throw new Error("Error creating course " + error);
+    }
+
 }
 
 export const getAllCoursesService = async () => {
-    return await prisma.course.findMany();
+    try {
+        const courses = await prisma.course.findMany();
+        return courses;
+    } catch (error) {
+        console.error(error);
+        throw new Error("Error getting all courses " + error);
+    }
+}
+
+export const assignUserToCourseService = async ({ userId, courseId }: { userId: string, courseId: string }) => {
+    try {
+        const member = await prisma.courseMember.create({
+            data: {
+                userId,
+                courseId
+            }
+        });
+        return member;
+    } catch (error) {
+        console.error(error);
+        throw new Error("Error assigning user to course " + error);
+    }
+
+}
+
+
+export const getCourseMembersService = async (courseId: string) => {
+    try {
+        const members = await prisma.courseMember.findMany({
+            where: {
+                courseId
+            },
+            include: {
+                user: true,
+                course: true
+            }
+        })
+        return members;
+    } catch (error) {
+        console.error(error);
+        throw new Error("Error getting course members " + error);
+    }
 }
